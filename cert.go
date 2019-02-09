4480a2a294627e130ea14cb05c01d45c091e08e2
chie4hao <w310930920@gmail.com>
2019-02-09 19:30:34 +0800

    add file

---
+++ cert.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"os"
+	"path"
+	"time"
+)
+
+const (
+	leafMaxAge = 24 * time.Hour
+	caUsage    = x509.KeyUsageDigitalSignature |
+		x509.KeyUsageContentCommitment |
+		x509.KeyUsageKeyEncipherment |
+		x509.KeyUsageDataEncipherment |
+		x509.KeyUsageKeyAgreement |
+		x509.KeyUsageCertSign |
+		x509.KeyUsageCRLSign
+	leafUsage = caUsage
+)
+
+var (
+	dir      = path.Join(os.Getenv("HOME"), ".mitm")
+	keyFile  = path.Join(dir, "ca_key.pem")
+	certFile = path.Join(dir, "ca_cert.pem")
+)
+
+func loadCA() (cert tls.Certificate, err error) {
+	cert, err = tls.LoadX509KeyPair(certFile, keyFile)
+	if os.IsNotExist(err) {
+		err = GenCA("mitm")
+		if err != nil {
+			cert, err = tls.LoadX509KeyPair(certFile, keyFile)
+		}
+	}
+	if err == nil {
+		cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
+	}
+	return
+}
+
+func genCert(ca *tls.Certificate, names []string) (*tls.Certificate, error) {
+	now := time.Now().Add(-1 * time.Hour).UTC()
+	if !ca.Leaf.IsCA {
+		return nil, errors.New("CA cert is not a CA")
+	}
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate serial number: %s", err)
+	}
+	tmpl := &x509.Certificate{
+		SerialNumber:          serialNumber,
+		Subject:               pkix.Name{CommonName: names[0]},
+		NotBefore:             now,
+		NotAfter:              now.Add(leafMaxAge),
+		KeyUsage:              leafUsage,
+		BasicConstraintsValid: true,
+		DNSNames:              names,
+		SignatureAlgorithm:    x509.ECDSAWithSHA512,
+	}
+	key, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	x, err := x509.CreateCertificate(rand.Reader, tmpl, ca.Leaf, key.Public(), ca.PrivateKey)
+	if err != nil {
+		return nil, err
+	}
+	cert := new(tls.Certificate)
+	cert.Certificate = append(cert.Certificate, x)
+	cert.PrivateKey = key
+	cert.Leaf, _ = x509.ParseCertificate(x)
+	return cert, nil
+}
+
+func GenCA(name string) (err error) {
+	now := time.Now().UTC()
+	caMaxAge := 5 * 365 * 24 * time.Hour
+	tmpl := &x509.Certificate{
+		SerialNumber:          big.NewInt(1),
+		Subject:               pkix.Name{CommonName: name},
+		NotBefore:             now,
+		NotAfter:              now.Add(caMaxAge),
+		KeyUsage:              caUsage,
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		MaxPathLen:            2,
+		SignatureAlgorithm:    x509.ECDSAWithSHA512,
+	}
+	key, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, tmpl, tmpl, key.Public(), key)
+	if err != nil {
+		return
+	}
+	keyDER, err := x509.MarshalECPrivateKey(key)
+	if err != nil {
+		return
+	}
+	certPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certDER,
+	})
+	keyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "ECDSA PRIVATE KEY",
+		Bytes: keyDER,
+	})
+
+	err = os.MkdirAll(dir, 0700)
+	if err != nil {
+		return
+	}
+
+	err = ioutil.WriteFile(certFile, certPEM, 0400)
+	if err == nil {
+		err = ioutil.WriteFile(keyFile, keyPEM, 0400)
+	}
+	return
+}
